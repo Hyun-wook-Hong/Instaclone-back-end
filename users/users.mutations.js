3b8b122248eee67c5b753fe8c6af045614851622
Jeremy Hong <hhw5321@gmail.com>
2021-05-20 23:40:49 +0900

    5/20 createAccount implements

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import client from "../client";
+import bcrypt from "bcrypt";
 
 /* 사용자 계정 생성 Mutation */
 export default{
@@ -20,9 +21,19 @@ export default{
                         ],
                     },
                 });                
-                // 2. hash password
+                // 2. hash password --> hashing 사용해서 암호화 + rainbow table 공격 방지를 위한 salting
+                const uglyPassword = await bcrypt.hash(password, 10);
                 
                 // 3. save and return the user
+                return client.user.create({ 
+                data: {
+                    username, 
+                    email, 
+                    firstName, 
+                    lastName, 
+                    password:uglyPassword,
+                } })
+
             },
-    }
-}
+    },
+};
